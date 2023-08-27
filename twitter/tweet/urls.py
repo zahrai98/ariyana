f1acc26795131a277abe01e89fa8830d8b6d27e1
zahrai98 <zahra.ms390@gmail.com>
2023-08-27 15:51:27 +0330

    add urls for posts to tweet app

---
+++ twitter/tweet/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import PostRetriveCreateDeleteAPIView, PostListAPIView, FollowingPostListAPIView
+
+
+app_name = 'tweet'
+urlpatterns = [
+    path('api/post/<int:post_id>/', PostRetriveCreateDeleteAPIView.as_view(), name='post'),
+    path('api/post/', PostListAPIView.as_view(), name='posts'),
+    path('api/followings_posts/', FollowingPostListAPIView.as_view(), name='followings_posts'),
+]
