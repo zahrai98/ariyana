01b37b8bd32778a3b35aee2e9c943720911ebf6d
zahrai98 <zahra.ms390@gmail.com>
2023-08-27 15:55:56 +0330

    add models to admin

--- twitter/account/admin.py
+++ twitter/account/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from . models import Relation
 # Register your models here.
+
+@admin.register(Relation)
+class RelationAdmin(admin.ModelAdmin):
+    pass
--- twitter/tweet/admin.py
+++ twitter/tweet/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from . models import Post, Comment, Vote
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    pass
