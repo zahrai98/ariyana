e06305a9c57fb251d0fdb22916a435dd13bffa5f
zahrai98 <zahra.ms390@gmail.com>
2023-08-27 15:40:51 +0330

    change in __str__ models and delete is_reply

--- twitter/tweet/models.py
+++ twitter/tweet/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
 		ordering = ['-created']
 
 	def __str__(self):
-		return f'{self.user} - {self.updated}'
+		return f'{self.body[:10]} - {self.updated}'
 
 	def likes_count(self):
 		return self.pvotes.count()
@@ -30,7 +30,6 @@ class Comment(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ucomments')
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pcomments')
 	reply = models.ForeignKey('self', on_delete=models.CASCADE, related_name='rcomments', blank=True, null=True)
-	is_reply = models.BooleanField(default=False)
 	body = models.TextField(max_length=400)
 	created = models.DateTimeField(auto_now_add=True)
 
@@ -43,5 +42,5 @@ class Vote(models.Model):
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='pvotes')
 
 	def __str__(self):
-		return f'{self.user} liked {self.post.slug}'
+		return f'{self.user} liked {self.post.body[:10]}'
 
